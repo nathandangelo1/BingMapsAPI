3c2d6c2e336044d17a3de38290514ea9b9ca10a8
Nate D. D'Angelo <69875151+nathandangelo1@users.noreply.github.com>
2023-05-09 13:57:11 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+﻿map = null;
+
+function LoadMap() {
+  map = new Microsoft.Maps.Map("#myMap", {});
+
+  //let mapCenter = map.getCenter();
+
+  Microsoft.Maps.Events.addHandler(map, "click", function (e) {
+    let loc = e.location;
+    AddPin(loc, 0, 0);
+  });
+
+  //   AddPin(mapCenter, 0, 0);
+  //   AddPin(mapCenter, 32, 32);
+} //end function
+
+function AddPin(location, offsetX = 0, offsetY = 0) {
+  //CREATE CUSTOM PUSHPIN
+  var pin = new Microsoft.Maps.Pushpin(location);
+
+  var offset = new Microsoft.Maps.Point(offsetX, offsetY);
+
+  //SET PINS DATA
+  let pinOptionsData = { icon: "images/ryu.gif", anchor: offset };
+  pin.setOptions(pinOptionsData);
+
+  //ADD THE PUSHPIN TO THE MAP
+  map.entities.push(pin);
+} //end function
