f2b36ae0de479ceceb7511a32051492a611203e3
Nate D. D'Angelo <69875151+nathandangelo1@users.noreply.github.com>
2023-05-20 12:52:20 -0500

    added sidenav. Working

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ var lat;
 var lon;
 const credentials =
   "AsnjENk9o2btta0rJzurVwsuleYaFxsWcc78p0Bop4TjK4M7PdNcjX1JyCXi6C45";
-
+var layer,
+  pageIdx = 0,
+  queryOptions,
+  numResults;
 //Query URL to the POI data source
 var sdsDataSourceUrl =
   "https://spatial.virtualearth.net/REST/v1/data/Microsoft/PointsOfInterest";
@@ -49,14 +52,19 @@ function GetMap() {
       }
     });
 
-  let button = document.getElementById("btnRestaurants");
-  button.addEventListener("click", function (e) {
-    let buttonValue = button.value;
-    NearbyLocations(buttonValue);
-  });
+  let buttons = document.getElementsByClassName("btnnearby");
+
+  for (var i = 0; i < buttons.length; i++) {
+    let button = buttons[i];
+    button.addEventListener("click", function (e) {
+      let buttonValue = button.value;
+      NearbyLocations(buttonValue);
+    });
+  }
 }
 
 function NearbyLocations(value) {
+  //map.layers.clear();
   //Create an infobox to display content for each result.
   infobox = new Microsoft.Maps.Infobox(map.getCenter(), { visible: false });
   infobox.setMap(map);
@@ -71,12 +79,12 @@ function NearbyLocations(value) {
     dest = m;
 
     infobox.setOptions({
-      htmlContent: `<div id="infoboxText" style="background: #e3b3b3; color: #000000; padding: 5px;">
+      htmlContent: `<div id="infoboxText" style="background: #f5f7f6; color: #000000; padding: 5px;">
       <div class="pull-right">
       <a id="ibox-close" class="infobox-close" onclick="closeInfoBox()">x
       </a></div><h2>${m.DisplayName.toString()}</h2><p>${
         m.AddressLine.toString() + ", " + m.Locality.toString()
-      }</p><button id="btn-getdirections2" type="button">Get Directions</button>
+      }</p><button id="btn-getdirections" type="button">Get Directions</button>
       </div>
       `,
       // title: m.DisplayName,
@@ -86,8 +94,10 @@ function NearbyLocations(value) {
     });
 
     document
-      .getElementById("btn-getdirections2")
+      .getElementById("btn-getdirections")
       .addEventListener("click", () => {
+        //map.layers.clear();
+        closeInfoBox();
         GetDirections();
       });
   });
@@ -95,7 +105,8 @@ function NearbyLocations(value) {
   //Load the Bing Spatial Data Services module.
   Microsoft.Maps.loadModule("Microsoft.Maps.SpatialDataService", function () {
     //Add an event handler for when the map moves.
-    Microsoft.Maps.Events.addHandler(map, "viewchangeend", getNearByLocations);
+    // Microsoft.Maps.Events.addHandler(map, "viewchangeend", getNearByLocations);
+    // Microsoft.Maps.Events.addHandler(map, "viewchangeend", getNearByLocations);
 
     //Trigger an initial search.
     getNearByLocations(value);
@@ -130,7 +141,7 @@ function Search(res) {
   r = res.resourceSets[0].resources;
   //Remove any previous results from the map.
   map.entities.clear();
-  layer.clear();
+  map.layers.clear();
 
   if (r && r.length > 0) {
     var pin,
@@ -140,17 +151,20 @@ function Search(res) {
 
     for (var i = 0; i < r.length; i++) {
       let coords = r[i].point.coordinates;
-      var location = new Microsoft.Maps.Location(coords[0], coords[1]);
-      //Create a pushpin for each result.
-      pin = new Microsoft.Maps.Pushpin(location, {
-        title: r[i].name,
-        subTitle: r[i].Address.addressLine,
-        text: i + 1 + "",
-      });
-      pins.push(pin);
-      locs.push(r[i].point.coordinates);
+      if (coords[0] !== "undefined" || coords[1] !== "undefined") {
+        const point = new Microsoft.Maps.Point(coords[0], coords[1]);
+        var location = new Microsoft.Maps.Location(coords[0], coords[1]);
+        //Create a pushpin for each result.
+        pin = new Microsoft.Maps.Pushpin(location, {
+          title: r[i].name,
+          subTitle: r[i].Address.addressLine,
+          text: i + 1 + "",
+        });
+        pins.push(pin);
+        locs.push(r[i].point.coordinates);
 
-      output += i + 1 + ") " + r[i].name + "<br/>";
+        output += i + 1 + ") " + r[i].name + "<br/>";
+      }
     }
 
     //Add the pins to the map
@@ -224,28 +238,155 @@ function getNearByLocations(value) {
   //Create a query to get nearby data.
   var queryOptions = {
     queryUrl: sdsDataSourceUrl,
-    top: 20,
+    top: 10,
+    inlineCount: true,
     spatialFilter: {
       spatialFilterType: "nearby",
       location: map.getCenter(),
-      radius: 15,
+      radius: 10,
     },
     filter: new Microsoft.Maps.SpatialDataService.Filter(
       "EntityTypeID",
       "eq",
-      value //restaurants
+      value //restaurants, hotels, nightlife
     ),
   };
 
-  //Process the query.
-  Microsoft.Maps.SpatialDataService.QueryAPIManager.search(
-    queryOptions,
-    map,
-    function (data) {
-      //Add results to the layer.
-      layer.add(data);
+  (queryOptions.skip = pageIdx * 10),
+    //Process the query.
+    Microsoft.Maps.SpatialDataService.QueryAPIManager.search(
+      queryOptions,
+      map,
+      function (data, inlineCount) {
+        //Add results to the layer.
+        //layer.add(data);
+
+        //Store the number of results available.
+        numResults = inlineCount;
+
+        if (data.length > 0) {
+          //Calculate the start and end result index.
+          var start = pageIdx * 10 + 1;
+          var end = start + data.length - 1;
+
+          document.getElementById("pageInfo").innerText =
+            "Results: " +
+            start +
+            " - " +
+            end +
+            " of " +
+            inlineCount +
+            " results";
+
+          //Create a list of the results.
+          var listHTML = ["<table>"],
+            locations = [];
+
+          for (var i = 0; i < data.length; i++) {
+            //Create HTML for each line item in the list.
+
+            //Add a column of index numbers.
+            listHTML.push("<tr><td>", start + i, ") </td>");
+
+            //Create a link that calls a function, pass in the EntityID of a result into the rel attribute for cross referencing the list item to the shape.
+            listHTML.push(
+              '<td><a href="javascript:void(0);" rel="',
+              data[i].metadata.EntityID,
+              '" ',
+              "onclick=\"listItemClicked('",
+              data[i].metadata.EntityID,
+              "');\">",
+              data[i].metadata.DisplayName,
+              "</a></td>"
+            );
+
+            //Create a column to display the distance to the location.
+            listHTML.push(
+              "<td>",
+              data[i].metadata.__Distance.toFixed(2),
+              " km(s)</td></tr>"
+            );
+
+            //Add the result number to the pushpin.
+            data[i].setOptions({ text: start + i + "" });
+
+            locations.push(data[i].getLocation());
+          }
+
+          listHTML.push("</table>");
+
+          document.getElementById("resultList").innerHTML = listHTML.join("");
+
+          //Add results to the data layer.
+          layer.add(data);
+
+          //Set the map view to show all the locations.
+          //Add padding to account for the pushpins pixel size.
+          map.setView({
+            bounds: Microsoft.Maps.LocationRect.fromLocations(locations),
+            padding: 30,
+          });
+          openNav();
+        }
+      }
+    );
+}
+
+function listItemClicked(entityId) {
+  //When an item in the list is clicked, look up its pushpin by entitiyId.
+  var shapes = layer.getPrimitives();
+
+  for (var i = 0; i < shapes.length; i++) {
+    if (shapes[i].metadata.EntityID == entityId) {
+      //Center the map over the pushpin and zoom in.
+      map.setView({ center: shapes[i].getLocation(), zoom: 15 });
+      break;
     }
-  );
+  }
+}
+
+function pageBackwards() {
+  if (pageIdx > 0) {
+    pageIdx--;
+    getNearByLocations();
+  }
+}
+
+function pageForward() {
+  //Ensure that paging does not exceed the number of results.
+  if ((pageIdx + 1) * 10 < numResults) {
+    pageIdx++;
+    getNearByLocations();
+  }
+}
+
+function highlightListItem(e) {
+  var shapeId = e.target.metadata.EntityID;
+
+  var elm = getListItemById(shapeId);
+
+  //Highlight the list item to indicate that its shape has been hovered.
+  elm.style.background = "LightGreen";
+
+  //Remove the highlighting after a second.
+  setTimeout(function () {
+    elm.style.background = "white";
+  }, 1000);
+}
+
+function getListItemById(shapeId) {
+  var listItems = document
+    .getElementById("resultList")
+    .getElementsByTagName("a");
+
+  for (var i = 0; i < listItems.length; i++) {
+    var rel = listItems[i].getAttribute("rel");
+    if (rel === shapeId) {
+      return listItems[i];
+    }
+  }
+
+  return null;
 }
 
 function selectedSuggestion(result) {
@@ -253,4 +394,15 @@ function selectedSuggestion(result) {
   map.entities.clear();
 
   map.setView({ bounds: result.bestView });
+}
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
